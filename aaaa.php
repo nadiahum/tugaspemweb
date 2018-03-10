2bc1f37669f1c9d2f1cb17ff772c50a485f862e7
nadiahum <37163666+nadiahum@users.noreply.github.com>
2018-03-10 11:19:31 +0800

    Add files via upload

---
+++ aaaa.php
@@ -0,0 +1,22 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+	<title>Applicant Data</title>
+	<link rel="stylesheet" type="text/css" href="bbbb.css">
+</head>
+<body>
+	<h1>Data User</h1>
+	<center><img src="taylor.jpg" width="150px" height="180px"></center>
+
+	<form>
+			<input type="text" placeholder="Nadia Humairo" >
+			<input type="text" placeholder="Banjarmasin, 18/05/1998">
+			<input type="text" placeholder="Female">
+			<input type="password" placeholder="nhnadiahumairo8@gmail.com">
+			<input type="password" placeholder="malangraya">
+			<input type="text" placeholder="+6281234268366">
+			<input type="text" placeholder="Faculty of Computer Science">
+			<input type="text" placeholder="Education">
+			<input type="address" placeholder="Jl. Anggrek Bulan, Number 12, Malang, East Java">
+</body>
+</html>
---
+++ abcd.php
@@ -0,0 +1,76 @@
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Form Registation</title>
+<link rel="stylesheet"" href="dcba.css">
+</head>
+<center>
+<body>
+<h3>FORM</h3>
+<Div class="aa">
+<form action="aaaa.php" method="get">
+<table>
+<tr>
+    <td>Name</td>
+    <td>:</td>
+    <td><input type="text" size="30"></td></tr>
+    <tr>
+    <td>Birthdate</td>
+    <td>:</td>
+    <td>
+    <input type="text" name="tempat" size="10">
+    <input type="text" name="tanggal" size="2"> / 
+    <input type="text" name="bulan" size="2"> / 
+    <input type="text" name="tahun" size="4"></td></tr>
+    <tr>
+    <td>Phone</td>
+    <td>:</td>
+    <td><input type="text" size="30"></td></tr>
+<tr>
+    <td>Email</td>
+    <td>:</td>
+    <td><input type="text" size="30">
+    <select value="email"><option>@gmail.com</option>
+                          <option>@gmail.co.id</option>
+                          <option>@yahoo.com</option>
+                          <option>@yahoo.co.id</option>
+                         </select>
+    </td>
+   </tr>
+<tr>
+    <td>Gender</td>
+    <td>:</td>
+    <td><select name="kelamin" value="sex">
+    <option>Female
+    <option>Male</td>
+<tr>
+    <td>Password</td>
+    <td>:</td>
+    <td><input type="password" size="30"></td>
+   </tr>
+<tr>
+    <td>Address</td>
+    <td>:</td>
+    <td><textarea cols="30" rows="5"></textarea></td>
+   </tr>
+   <tr>
+    <td>Majors</td>
+    <td>:</td>
+    <td><select name="Majors">
+      <option>FILKOM</option>
+      <option>FEB</option>
+      <option>FISIP</option>
+      <option>KEDOKTERAN</option>
+    </select></td>
+   </tr>
+   <tr>
+    <td>Upload Photo</td>
+    <td>:</td>
+    <td><input type="file" value="upload gambar"/></td>
+   </tr>
+</table><br>
+<input type="checkbox"> I agree with terms and service <br><br>
+<input type="submit" class="submit" value="SUBMIT" cols="30"></form></div></div>
+</body>
+</center>
+</html>
